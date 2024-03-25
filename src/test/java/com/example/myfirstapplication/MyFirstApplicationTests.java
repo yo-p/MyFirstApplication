7b42e6bdab9492632ddc8de60dadc6cdc0d1ef73
yo-p <papapapipipi42@gmail.com>
2024-03-25 22:58:38 +0900

    初回コミット

---
+++ src/test/java/com/example/myfirstapplication/MyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
