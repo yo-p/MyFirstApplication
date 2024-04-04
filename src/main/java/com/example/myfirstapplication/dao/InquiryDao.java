5273594006055a1dacd7522128de1c4bd082397a
yo-p <papapapipipi42@gmail.com>
2024-04-04 23:36:05 +0900

    セクション6-40まで完了

---
+++ src/main/java/com/example/myfirstapplication/dao/InquiryDao.java
@@ -0,0 +1,10 @@
+package com.example.myfirstapplication.dao;
+
+import com.example.myfirstapplication.entity.Inquiry;
+
+import java.util.List;
+
+public interface InquiryDao {
+    void insertInquiry(Inquiry inquiry);
+    List<Inquiry> getAll();
+}
